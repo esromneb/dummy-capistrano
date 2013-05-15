6329200985b9a7619bdd7fa24876f97883ed5a73
Benjamin Morse <bjmorse@gmail.com>
2013-05-15 01:09:48 -0700

    * adding base rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DummyCapistrano::Application.config.secret_token = '376ed3b4d23e597bced92c4b0ed2e17c1e568e2c844687cd9e1dc8a95716c1b6d83f5e759c7a4e6d5f02abc33fc1879646036267eb182412aaa733de6d9aa57f'
